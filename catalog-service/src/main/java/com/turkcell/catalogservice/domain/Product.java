93848435b14bfd5a113b96b04f10d302624d52a2
halitklyc <halitenes.kalayci@gmail.com>
2025-10-06 15:58:40 +0300

    ddd impl. v2

--- catalog-service/src/main/java/com/turkcell/catalogservice/domain/Product.java
+++ catalog-service/src/main/java/com/turkcell/catalogservice/domain/Product.java
@@ -6,8 +6,56 @@ import java.time.OffsetDateTime;
 public class Product {
     private final ProductId id;
     private String name;
+    private Money money;
     private String description;
     private OffsetDateTime createdAt;
     private OffsetDateTime updatedAt;
     private int stock;
+    // Kontrollü Üretim
+    private Product(ProductId id,
+                   String name,
+                   String description,
+                   Money money,
+                   int stock,
+                   OffsetDateTime createdAt,
+                   OffsetDateTime updatedAt
+                   ) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.stock = stock;
+        this.money = money;
+    }
+
+    // Static Factory Method -
+    // TODO: Rehydrate Method
+    public static Product create(String name,
+                                 String description,
+                                 Money money,
+                                 int stock) {
+        return new Product(
+                ProductId.newId(),
+                name,
+                description,
+                money,
+                stock,
+                OffsetDateTime.now(),
+                null
+        );
+    }
+
+    public void dispatch(int quantity)
+    {
+        if(quantity <= 0)
+            throw new IllegalArgumentException("Quantity must be greater than zero");
+
+        if(quantity > stock)
+            throw new IllegalArgumentException("Quantity must be less than stock");
+
+        this.stock -= quantity;
+    }
 }
+// Action-Based -> dispatch(10), restock(20); setName X rename()
+// Setter -> setStock(10); Değer override eder.
