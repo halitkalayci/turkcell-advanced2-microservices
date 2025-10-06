6522905aaa2c3b43dfc49c1b51e90af8e5163982
halitklyc <halitenes.kalayci@gmail.com>
2025-10-06 11:47:29 +0300

    deprecated ve sunset için headerlar eklendi.

--- catalog-service/src/main/java/com/turkcell/catalogservice/CatalogController.java
+++ catalog-service/src/main/java/com/turkcell/catalogservice/CatalogController.java
@@ -4,6 +4,8 @@ import com.turkcell.catalogservice.contract.v1.api.CatalogApi;
 import com.turkcell.catalogservice.contract.v1.model.ListProducts200Response;
 import com.turkcell.catalogservice.contract.v1.model.ProductCreateRequest;
 import com.turkcell.catalogservice.contract.v1.model.ProductResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +15,11 @@ import java.util.UUID;
 public class CatalogController implements CatalogApi {
     @Override
     public ResponseEntity<ProductResponse> createProduct(ProductCreateRequest productCreateRequest) {
-        return null;
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Deprecation", "true");
+        headers.set("Sunset", "Tue, 31 Dec 2025 23:59:59 GMT");
+
+        return new ResponseEntity<>(new ProductResponse(), headers, HttpStatus.CREATED);
     }
 
     @Override
