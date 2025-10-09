4d71dd8137379c473136da168c5a39bd5be9ed74
halitklyc <halitenes.kalayci@gmail.com>
2025-10-09 10:45:55 +0300

    /api/me endpointi eklendi.

---
+++ bff/src/main/java/com/turkcell/bff/controller/BffController.java
@@ -0,0 +1,27 @@
+package com.turkcell.bff.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class BffController {
+
+    @GetMapping("/me")
+    public Mono<Map<String,Object>> me(@AuthenticationPrincipal OidcUser user) {
+        return Mono.just(
+                Map.of(
+                        "sub", user.getSubject(),
+                        "username", user.getPreferredUsername(),
+                        "email", user.getEmail(),
+                        "roles", user.getAuthorities()
+                )
+        );
+    }
+}
